25eed72540db399557c10aaabeadd54e481afb27
carolchristmashan <carolchristmashan@gmail.com>
2025-09-30 09:33:57 -0400

    added getting started code

---
+++ game.py
@@ -0,0 +1,27 @@
+#import libraries
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("blue")
+
+    # RENDER YOUR GAME HERE
+    pygame.display.flip()
+
+    # flip() the display to put your work on screen
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
